741a05945b800d224bf112d58975ed1277a49d32
chaoticpotato <omer@jotform.com>
2024-06-13 17:33:41 +0300

    cohorts

--- src/CohortInfo.jsx
+++ src/CohortInfo.jsx
@@ -1,6 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const formatDate = (isoDate) => {
+  const dateObj = new Date(isoDate);
+
+  const day = dateObj.getUTCDate();
+  const monthIndex = dateObj.getUTCMonth();
+  const year = dateObj.getUTCFullYear();
+
+  const months = [
+    "Ocak",
+    "Şubat",
+    "Mart",
+    "Nisan",
+    "Mayıs",
+    "Haziran",
+    "Temmuz",
+    "Ağustos",
+    "Eylül",
+    "Ekim",
+    "Kasım",
+    "Aralık",
+  ];
+
+  return `${day} ${months[monthIndex]} ${year}`;
+};
+
 export default function CohortInfo({ cohortId, setCurrentSprint }) {
   const {
     data: cohortInfo,
@@ -20,11 +45,23 @@ export default function CohortInfo({ cohortId, setCurrentSprint }) {
 
   isSuccess && setCurrentSprint(lastFinishedSprint?.sprint_order);
 
+  const pDate = isSuccess
+    ? formatDate(lastFinishedSprint?.planning_date)
+    : null;
+
   return (
-    <div className="border-y border-black/10 p-2 text-sm">
+    <div className="border-y border-black/10 py-2 px-3 text-sm">
       <h2 className="font-bold">Cohort Bilgisi</h2>
-      <div>Güncel Cohort: {lastFinishedSprint?.sprint_order}</div>
-      <div>Güncel Eğitmen: {lastFinishedSprint?.instructor}</div>
+      <div>
+        Güncel Cohort:{" "}
+        <span className="font-bold">
+          {lastFinishedSprint?.sprint_order} - {pDate}
+        </span>
+      </div>
+      <div>
+        Güncel Eğitmen:{" "}
+        <span className="font-bold">{lastFinishedSprint?.instructor}</span>
+      </div>
     </div>
   );
 }
