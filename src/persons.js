c7efc85e697c1dec631641b7c3e5775d3edc89f4
chaoticpotato <omer@jotform.com>
2024-06-26 19:32:30 +0300

    0624 added

--- src/persons.js
+++ src/persons.js
@@ -5,6 +5,7 @@ import { p0224 } from "./persons/p0224";
 import { p0324 } from "./persons/p0324";
 import { p0424 } from "./persons/p0424";
 import { p0524 } from "./persons/p0524";
+import { p0624 } from "./persons/p0624";
 
 export const cohortIds = {
   1123: 17,
@@ -15,6 +16,7 @@ export const cohortIds = {
   "0224": 26,
   "0324-0424": 33,
   "0524": 36,
+  "0624": 38,
 };
 
 export const javaCohorts = [29, 37];
@@ -28,4 +30,5 @@ export const persons = {
   "java_01+02": [...p0124, ...p0224],
   "0324-0424": [...p0324, ...p0424],
   "0524": p0524,
+  "0624": p0624,
 };
