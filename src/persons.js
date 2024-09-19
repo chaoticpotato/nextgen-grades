04a88563b10a4fd446d546dc0f279f561a8a535a
chaoticpotato <omer@jotform.com>
2024-09-19 14:38:01 +0300

    frontend2024

--- src/persons.js
+++ src/persons.js
@@ -3,6 +3,7 @@ import { p1223 } from "./persons/p1223";
 import { p0124 } from "./persons/p0124";
 import { p0224 } from "./persons/p0224";
 import { fe0424 } from "./persons/fe0424";
+import { frontend2024 } from "./persons/frontend2024";
 
 
 export const cohortIds = {
@@ -22,10 +23,11 @@ export const cohortIds = {
 };
 
 export const javaCohorts = [29, 37];
-export const javaCohortNames = ["java_11+12", "java_01+02", "0424-FE"];
+export const javaCohortNames = ["java_11+12", "java_01+02", "0424-FE", "frontend2024"];
 
 export const persons = {
   "java_11+12": [...p1123, ...p1223],
   "java_01+02": [...p0124, ...p0224],
   "0424-FE": fe0424,
+  "frontend2024": frontend2024
 };
---
+++ src/persons/frontend2024.js
@@ -0,0 +1,42 @@
+export const frontend2024 = [
+    {
+        "id": 198663,
+        "name": "Ferhat Genç",
+        "email": "ferhatgenc12@gmail.com"
+    },
+    {
+        "id": 198586,
+        "name": "Ayşem Uğur",
+        "email": "aysemugr@gmail.com"
+    },
+    {
+        "id": 198585,
+        "name": "Berke Fener",
+        "email": "fenerberke4@gmail.com"
+    },
+    {
+        "id": 198583,
+        "name": "Elif Akan",
+        "email": "elif.akan.2002@hotmail.com"
+    },
+    {
+        "id": 193026,
+        "name": "Koray Bozefe",
+        "email": "kbozefe@gmail.com"
+    },
+    {
+        "id": 193025,
+        "name": "Uygar Akın",
+        "email": "uygarakin-fb@hotmail.com"
+    },
+    {
+        "id": 193022,
+        "name": "Tuğba Taşkın",
+        "email": "inbox.tugba@outlook.com"
+    },
+    {
+        "id": 182697,
+        "name": "Seçil Yücel",
+        "email": "seecilyuceel@gmail.com"
+    }
+]
